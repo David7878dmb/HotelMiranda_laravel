7c032a6bcc0a2d1ac545fd78f84ccae0631398a4
David7878.dmb <david7878.dmb@gmail.com>
2024-11-11 16:25:36 +0100

    Feat: Activitty Seeder

---
+++ database/seeders/ActivitySeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Activity;  
+use Faker\Factory as FakerFactory;
+
+class ActivitySeeder extends Seeder
+{
+    public function run(): void
+    {
+        $faker = FakerFactory::create('es_ES');
+
+        $users = User::all();
+
+        $activities = [];
+
+        for ($i = 0; $i<20; $i++){
+            $activities[] = [
+                'type' => $faker->randomElement(['surf', 'windsurf', 'kayak', 'atv', 'hot air balloon']),
+                'user_id' => $users->random()->id,
+                'datetime' => $faker->dateTimeBetween('-1 year', 'now'),
+                'paid' => $faker->boolean(70),
+                'notes' => $faker->text(100),
+                'satisfaction' => $faker->numbeBetween(1, 5),
+            ];
+        }
+        Activity::insert($activities);
+    }
+}
