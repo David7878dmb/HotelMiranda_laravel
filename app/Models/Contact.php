05b2ca889a8cd55b4a8751e5b26819d1c180e8a8
David7878.dmb <david7878.dmb@gmail.com>
2024-11-13 16:04:26 +0100

    Feat: Fillable data Contact

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -3,8 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Contact extends Model
 {
-    //
+    protected $fillable=[
+        'name',
+        'phone',
+        'email',
+        'subject',
+        'text',
+        'read'
+    ];
 }
