d53c6a0cdb59f4ba503b0b36450f6d0a752c56e4
David7878.dmb <david7878.dmb@gmail.com>
2024-11-14 11:21:49 +0100

    Feat: Protected class booking

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -3,8 +3,23 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Booking extends Model
 {
-    //
+    protected $filleable = [
+        'guest',
+        'picture',
+        'order_date',
+        'check_in',
+        'check_out',
+        'discount',
+        'notes',
+        'room_id',
+        'status'
+    ];
+
+    public function rooms() :BelongsTo{
+        return $this->belongsTo(Room::class);
+    }
 }
